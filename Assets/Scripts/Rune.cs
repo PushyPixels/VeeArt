c13e26391e9529218fb6adc7022e7f2f6a6dc35e
Maximilian Maroe <drakfyre@gmail.com>
2021-11-04 16:01:10 -0700

    Simplified method

--- Assets/Scripts/Rune.cs
+++ Assets/Scripts/Rune.cs
@@ -9,31 +9,4 @@ public class Rune : MonoBehaviour
     {
         Debug.Log("Exec called on: " + gameObject.name, gameObject);
     }
-
-    virtual public void Return()
-    {
-        Debug.Log("Exec called on: " + gameObject.name, gameObject);
-    }
-
-    [Serializable]
-    public class Parameter<T>
-    {
-        [SerializeField]
-        public string Name { get; set; }
-        [SerializeField]
-        public T Value { get; set; }
-
-        public Parameter(string name)
-        {
-            Name = name;
-        }
-
-        public Type ParameterType
-        {
-            get
-            {
-                return typeof(T);
-            }
-        }
-    }
 }
