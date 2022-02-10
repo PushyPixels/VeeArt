620d3b6d56972ebe69271af157efcb5f7a0081f7
Tim Coate <encryptedprinter@gmail.com>
2022-02-10 06:36:01 -0800

    Reorganizing Save and Load routines into Rune parent class

--- Assets/Scripts/Runes/BaseClasses/Rune.cs
+++ Assets/Scripts/Runes/BaseClasses/Rune.cs
@@ -1,7 +1,9 @@
 using System;
+using System.IO;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using MessagePack;
 
 public class Rune : RuneBase
 {
@@ -16,6 +18,30 @@ public class Rune : RuneBase
             //Debug.Log("Calling Exec on: " + nextExec.name, nextExec.gameObject);
             nextExec.Exec();
         }
+    }
+
+    protected MementoType Load<MementoType>()
+    {
+        string path = Path.Combine(Application.persistentDataPath, guid + ".rune");
+        if(File.Exists(path))
+        {
+            byte[] bytes = File.ReadAllBytes(path);
+            var lz4Options = MessagePackSerializerOptions.Standard.WithCompression(MessagePackCompression.Lz4BlockArray);
+            return MessagePackSerializer.Deserialize<MementoType>(bytes, lz4Options);
+        }
+        else
+        {
+            Debug.LogWarning("No serialized data found, using data from Scene file.", gameObject);
+            return default(MementoType);
+        }
+    }
+
+    protected void Save<MementoType>(MementoType memento)
+    {
+        string path = Path.Combine(Application.persistentDataPath, guid + ".rune");
+        var lz4Options = MessagePackSerializerOptions.Standard.WithCompression(MessagePackCompression.Lz4BlockArray);
+        byte[] bytes = MessagePackSerializer.Serialize(memento, lz4Options);
+        File.WriteAllBytes(path, bytes);
     }
 
     protected override void OnValidate()
--- Assets/Scripts/Runes/Vector3List.cs
+++ Assets/Scripts/Runes/Vector3List.cs
@@ -1,4 +1,4 @@
-using System;
+ using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.IO;
@@ -11,46 +11,29 @@ public class Vector3List : Rune
     
     private List<Vector3> vectorList = new List<Vector3>();
 
-    private Vector3ListMememto memento = new Vector3ListMememto();
+    private Vector3ListMememto memento;
 
     void Start()
     {
-        Load();
+        memento = Load<Vector3ListMememto>();
+        if(memento != null)
+        {
+            vector3ListOutput.connectedRune = RuneBase.FindRuneByGuid(memento.vector3ListOutputGuid) as RuneParameter<List<Vector3>>;
+            vectorList = memento.vectorList;
+        }
+
         vector3ListOutput.value = vectorList;
     }
 
     void OnApplicationQuit()
     {
-        Save();
-    }
-
-    void Load()
-    {
-        string path = Path.Combine(Application.persistentDataPath, guid + ".rune");
-        if(File.Exists(path))
-        {
-            byte[] bytes = File.ReadAllBytes(path);
-            var lz4Options = MessagePackSerializerOptions.Standard.WithCompression(MessagePackCompression.Lz4BlockArray);
-            memento = MessagePackSerializer.Deserialize<Vector3ListMememto>(bytes, lz4Options);
-
-            vector3ListOutput.connectedRune = RuneBase.FindRuneByGuid(memento.vector3ListOutputGuid) as RuneParameter<List<Vector3>>;
-            vectorList = memento.vectorList;
+        if(memento == null)
+        { 
+            memento = new Vector3ListMememto();
         }
-        else
-        {
-            Debug.LogWarning("No serialized data found, using data from Scene file.", gameObject);
-        }
-    }
-
-    void Save()
-    {
         memento.vector3ListOutputGuid = vector3ListOutput.guid;
         memento.vectorList = vectorList;
-
-        string path = Path.Combine(Application.persistentDataPath, guid + ".rune");
-        var lz4Options = MessagePackSerializerOptions.Standard.WithCompression(MessagePackCompression.Lz4BlockArray);
-        byte[] bytes = MessagePackSerializer.Serialize(memento, lz4Options);
-        File.WriteAllBytes(path, bytes);
+        Save<Vector3ListMememto>(memento);
     }
 }
 
